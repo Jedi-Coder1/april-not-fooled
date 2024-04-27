dee68ce1b57d280481d47626aecd2f53bef7bade
Jedi-Coder1 <98861141+Jedi-Coder1@users.noreply.github.com>
2024-04-27 13:51:40 -0600

    function naming

--- didntfool.js
+++ didntfool.js
@@ -1,50 +1,36 @@
 window.addEventListener("load", function() {
-    function i(e) {
-        var e = e.getBoundingClientRect(),
+    function isVisble(element) {
+        var elementRect = element.getBoundingClientRect(),
             t = window.innerHeight || document.documentElement.clientHeight,
             n = window.innerWidth || document.documentElement.clientWidth;
-        return 0 < e.bottom && 0 < e.right && e.top < t && e.left < n
+        return 0 < elementRect.bottom && 0 < elementRect.right && elementRect.top < t && elementRect.left < n
     }
 
-    function d(e) {
+    function shortsHelper(e) {
         var t = e.split("/shorts/", 2);
         return (e = 2 === (t = (e = 2 === t.length ? t[1] : t[0]).split("v=", 2)).length ? t[1] : t[0]).split("&", 1)[0]
     }
 
-    function o(e, t) {
+    function fetchVideoInfo(videoID, t) {
         fetch("https://www.youtube.com/youtubei/v1/player?prettyPrint=false", {
             method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                context: {
-                    client: {
-                        clientName: "WEB",
-                        clientVersion: "2.20240416.01.00"
-                    }
-                },
-                videoId: e
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({context: {client: {clientName: "WEB",clientVersion: "2.20240416.01.00"}},
+                videoId: videoID
             })
-        }).then(e => {
-            if (e.ok) return e.json();
+        }).then(res => {
+            if (res.ok) return res.json();
             throw new Error("Network response was not ok")
-        }).then(e => {
-            e = e.microformat.playerMicroformatRenderer;
-            e.liveBroadcastDetails?.isLiveNow ? t(e.liveBroadcastDetails.startTimestamp) : e.publishDate ? t(e.publishDate) : t(e.uploadDate)
-        }).catch(e => {
-            console.error("There was a problem with the fetch operation:", e)
-        })
+        }).then(res => {
+            res = res.microformat.playerMicroformatRenderer;
+            res.liveBroadcastDetails?.isLiveNow ? t(res.liveBroadcastDetails.startTimestamp) : res.publishDate ? t(res.publishDate) : t(res.uploadDate)
+        }).catch(res => {console.error("There was a problem with the fetch operation:", res)})
     }
 
-    function c(e) {
+    function LocalizeDates(e) {
         var e = new Date(e),
             t = navigator.language || "en-US";
-        return e.toLocaleDateString(t, {
-            day: "numeric",
-            month: "short",
-            year: "numeric"
-        })
+        return e.toLocaleDateString(t, {day: "numeric", month: "short", year: "numeric"})
     }
 
     function t() {
@@ -58,7 +44,7 @@ window.addEventListener("load", function() {
                 }
                 return null
             }()) {
-            var t, n = c(n);
+            var t, n = LocalizeDates(n);
             (function() {
                 var e = document.body.querySelector("player-microformat-renderer script");
                 if (!e) return null;
@@ -77,8 +63,8 @@ window.addEventListener("load", function() {
                 let t;
                 var a = (t = 1 === n.length ? ((r = document.createElement("span")).className = "inline-metadata-item style-scope ytd-video-meta-block", a = document.createTextNode(""), r.appendChild(a), n[0].insertAdjacentElement("afterend", r), r) : n[1]).firstChild.nodeValue,
                     r = t.getAttribute("data-text");
-                null !== r && r == a || i(t) && (t.setAttribute("data-text", a), n = d(e.querySelector("a#thumbnail").getAttribute("href")), s && u(n, "video page sidebar list"), o(n, e => {
-                    e = c(e), t.firstChild.nodeValue = e, t.setAttribute("data-text", e)
+                null !== r && r == a || isVisble(t) && (t.setAttribute("data-text", a), n = shortsHelper(e.querySelector("a#thumbnail").getAttribute("href")), s && utility(n, "video page sidebar list"), fetchVideoInfo(n, e => {
+                    e = LocalizeDates(e), t.firstChild.nodeValue = e, t.setAttribute("data-text", e)
                 }))
             }
         }), 0 !== (e = document.querySelectorAll("#contents > ytd-rich-grid-row > #contents > ytd-rich-item-renderer")).length && e.forEach(e => {
@@ -87,8 +73,8 @@ window.addEventListener("load", function() {
                 let t;
                 var a = (t = 1 === n.length ? ((r = document.createElement("span")).className = "inline-metadata-item style-scope ytd-video-meta-block", a = document.createTextNode(""), r.appendChild(a), n[0].insertAdjacentElement("afterend", r), r) : n[1]).firstChild.nodeValue,
                     r = t.getAttribute("data-text");
-                null !== r && r === a || i(t) && (t.setAttribute("data-text", a), n = d(e.querySelector("a#thumbnail").getAttribute("href")), s && u(n, "homepage list - videos"), o(n, e => {
-                    e = c(e), t.firstChild.nodeValue = e, t.setAttribute("data-text", e)
+                null !== r && r === a || isVisble(t) && (t.setAttribute("data-text", a), n = shortsHelper(e.querySelector("a#thumbnail").getAttribute("href")), s && utility(n, "homepage list - videos"), fetchVideoInfo(n, e => {
+                    e = LocalizeDates(e), t.firstChild.nodeValue = e, t.setAttribute("data-text", e)
                 }))
             }
         }), 0 !== (e = document.querySelectorAll("ytd-rich-shelf-renderer > #dismissible > #contents > ytd-rich-item-renderer")).length && e.forEach(e => {
@@ -97,8 +83,8 @@ window.addEventListener("load", function() {
                 let t;
                 var a = (t = 1 === n.length ? ((r = document.createElement("span")).className = "inline-metadata-item style-scope ytd-video-meta-block", a = document.createTextNode(""), r.appendChild(a), n[0].insertAdjacentElement("afterend", r), r) : n[1]).firstChild.nodeValue,
                     r = t.getAttribute("data-text");
-                null !== r && r === a || i(t) && (t.setAttribute("data-text", a), n = d(e.querySelector("a#thumbnail").getAttribute("href")), s && u(n, "homepage list - shorts"), o(n, e => {
-                    e = c(e), t.firstChild.nodeValue = e, t.setAttribute("data-text", e)
+                null !== r && r === a || isVisble(t) && (t.setAttribute("data-text", a), n = shortsHelper(e.querySelector("a#thumbnail").getAttribute("href")), s && utility(n, "homepage list - shorts"), fetchVideoInfo(n, e => {
+                    e = LocalizeDates(e), t.firstChild.nodeValue = e, t.setAttribute("data-text", e)
                 }))
             }
         }), 0 !== (e = document.querySelectorAll("#contents > ytd-item-section-renderer > #contents > ytd-video-renderer")).length && e.forEach(e => {
@@ -107,8 +93,8 @@ window.addEventListener("load", function() {
                 let t;
                 var a = (t = 1 === n.length ? ((r = document.createElement("span")).className = "inline-metadata-item style-scope ytd-video-meta-block", a = document.createTextNode(""), r.appendChild(a), n[0].insertAdjacentElement("afterend", r), r) : n[1]).firstChild.nodeValue,
                     r = t.getAttribute("data-text");
-                null !== r && r === a || i(t) && (t.setAttribute("data-text", a), n = d(e.querySelector("a#thumbnail").getAttribute("href")), s && u(n, "search list - videos"), o(n, e => {
-                    e = c(e), t.firstChild.nodeValue = e, t.setAttribute("data-text", e)
+                null !== r && r === a || isVisble(t) && (t.setAttribute("data-text", a), n = shortsHelper(e.querySelector("a#thumbnail").getAttribute("href")), s && utility(n, "search list - videos"), fetchVideoInfo(n, e => {
+                    e = LocalizeDates(e), t.firstChild.nodeValue = e, t.setAttribute("data-text", e)
                 }))
             }
         }), 0 !== (e = document.querySelectorAll("#scroll-container > #items > ytd-reel-item-renderer")).length && e.forEach(e => {
@@ -117,8 +103,8 @@ window.addEventListener("load", function() {
                 let t;
                 var a = (t = 1 === n.length ? ((r = document.createElement("span")).className = "inline-metadata-item style-scope ytd-video-meta-block", a = document.createTextNode(""), r.appendChild(a), n[0].insertAdjacentElement("afterend", r), r) : n[1]).firstChild.nodeValue,
                     r = t.getAttribute("data-text");
-                null !== r && r === a || i(t) && (t.setAttribute("data-text", a), n = d(e.querySelector("a#thumbnail").getAttribute("href")), s && u(n, "search list - shorts"), o(n, e => {
-                    e = c(e), t.firstChild.nodeValue = e, t.setAttribute("data-text", e)
+                null !== r && r === a || isVisble(t) && (t.setAttribute("data-text", a), n = shortsHelper(e.querySelector("a#thumbnail").getAttribute("href")), s && utility(n, "search list - shorts"), fetchVideoInfo(n, e => {
+                    e = LocalizeDates(e), t.firstChild.nodeValue = e, t.setAttribute("data-text", e)
                 }))
             }
         }), 0 !== (e = document.querySelectorAll("ytd-shelf-renderer > #dismissible > #contents > ytd-vertical-list-renderer > #items > ytd-video-renderer")).length && e.forEach(e => {
@@ -127,8 +113,8 @@ window.addEventListener("load", function() {
                 let t;
                 var a = (t = 1 === n.length ? ((r = document.createElement("span")).className = "inline-metadata-item style-scope ytd-video-meta-block", a = document.createTextNode(""), r.appendChild(a), n[0].insertAdjacentElement("afterend", r), r) : n[1]).firstChild.nodeValue,
                     r = t.getAttribute("data-text");
-                null !== r && r === a || i(t) && (t.setAttribute("data-text", a), n = d(e.querySelector("a#thumbnail").getAttribute("href")), s && u(n, "search list - people also watched"), o(n, e => {
-                    e = c(e), t.firstChild.nodeValue = e, t.setAttribute("data-text", e)
+                null !== r && r === a || isVisble(t) && (t.setAttribute("data-text", a), n = shortsHelper(e.querySelector("a#thumbnail").getAttribute("href")), s && utility(n, "search list - people also watched"), fetchVideoInfo(n, e => {
+                    e = LocalizeDates(e), t.firstChild.nodeValue = e, t.setAttribute("data-text", e)
                 }))
             }
         }), 0 !== (e = document.querySelectorAll("#lists > ytd-vertical-watch-card-list-renderer > #items > ytd-watch-card-compact-video-renderer")).length && e.forEach(e => {
@@ -140,8 +126,8 @@ window.addEventListener("load", function() {
                     let t = a[0] + " • ";
                     var a = a[1],
                         r = n.getAttribute("data-text");
-                    null !== r && r === a || i(n) && (n.setAttribute("data-text", a), r = d(e.querySelector("a#thumbnail").getAttribute("href")), s && u(r, "search list - topic in sidebar"), o(r, e => {
-                        e = c(e), n.firstChild.nodeValue = t + e, n.setAttribute("data-text", e)
+                    null !== r && r === a || isVisble(n) && (n.setAttribute("data-text", a), r = shortsHelper(e.querySelector("a#thumbnail").getAttribute("href")), s && utility(r, "search list - topic in sidebar"), fetchVideoInfo(r, e => {
+                        e = LocalizeDates(e), n.firstChild.nodeValue = t + e, n.setAttribute("data-text", e)
                     }))
                 }
             }
@@ -151,8 +137,8 @@ window.addEventListener("load", function() {
                 let t;
                 var a = (t = 1 === n.length ? ((r = document.createElement("span")).className = "inline-metadata-item style-scope ytd-video-meta-block", a = document.createTextNode(""), r.appendChild(a), n[0].insertAdjacentElement("afterend", r), r) : n[1]).firstChild.nodeValue,
                     r = t.getAttribute("data-text");
-                null !== r && r === a || i(t) && (t.setAttribute("data-text", a), n = d(e.querySelector("a#thumbnail").getAttribute("href")), s && u(n, "channel page - home (featured video)"), o(n, e => {
-                    e = c(e), t.firstChild.nodeValue = e, t.setAttribute("data-text", e)
+                null !== r && r === a || isVisble(t) && (t.setAttribute("data-text", a), n = shortsHelper(e.querySelector("a#thumbnail").getAttribute("href")), s && utility(n, "channel page - home (featured video)"), fetchVideoInfo(n, e => {
+                    e = LocalizeDates(e), t.firstChild.nodeValue = e, t.setAttribute("data-text", e)
                 }))
             }
         }), 0 !== (e = document.querySelectorAll("#contents > ytd-item-section-renderer > #contents > ytd-channel-video-player-renderer")).length && e.forEach(e => {
@@ -161,8 +147,8 @@ window.addEventListener("load", function() {
                 let t;
                 var a = (t = 1 === n.length ? ((r = document.createElement("span")).className = "inline-metadata-item style-scope ytd-video-meta-block", a = document.createTextNode(""), r.appendChild(a), n[0].insertAdjacentElement("afterend", r), r) : n[1]).firstChild.nodeValue,
                     r = t.getAttribute("data-text");
-                null !== r && r === a || i(t) && (t.setAttribute("data-text", a), n = d(e.querySelector("#metadata-container > yt-formatted-string > a.yt-simple-endpoint").getAttribute("href")), s && u(n, "channel page - home (featured video)"), o(n, e => {
-                    e = c(e), t.firstChild.nodeValue = e, t.setAttribute("data-text", e)
+                null !== r && r === a || isVisble(t) && (t.setAttribute("data-text", a), n = shortsHelper(e.querySelector("#metadata-container > yt-formatted-string > a.yt-simple-endpoint").getAttribute("href")), s && utility(n, "channel page - home (featured video)"), fetchVideoInfo(n, e => {
+                    e = LocalizeDates(e), t.firstChild.nodeValue = e, t.setAttribute("data-text", e)
                 }))
             }
         }), 0 !== (e = document.querySelectorAll("#scroll-container > #items > ytd-grid-video-renderer")).length && e.forEach(e => {
@@ -171,8 +157,8 @@ window.addEventListener("load", function() {
                 let t;
                 var a = (t = 1 === n.length ? ((r = document.createElement("span")).className = "style-scope ytd-grid-video-renderer", a = document.createTextNode(""), r.appendChild(a), n[0].insertAdjacentElement("afterend", r), r) : n[1]).firstChild.nodeValue,
                     r = t.getAttribute("data-text");
-                null !== r && r === a || i(t) && (t.setAttribute("data-text", a), n = d(e.querySelector("a#thumbnail").getAttribute("href")), s && u(n, "channel page - home (for you videos)"), o(n, e => {
-                    e = c(e), t.firstChild.nodeValue = e, t.setAttribute("data-text", e)
+                null !== r && r === a || isVisble(t) && (t.setAttribute("data-text", a), n = shortsHelper(e.querySelector("a#thumbnail").getAttribute("href")), s && utility(n, "channel page - home (for you videos)"), fetchVideoInfo(n, e => {
+                    e = LocalizeDates(e), t.firstChild.nodeValue = e, t.setAttribute("data-text", e)
                 }))
             }
         }), 0 !== (e = document.querySelectorAll("#contents > ytd-playlist-video-list-renderer > #contents > ytd-playlist-video-renderer")).length && e.forEach(e => {
@@ -182,8 +168,8 @@ window.addEventListener("load", function() {
                 2 === a.length ? ((l = document.createElement("span")).className = "style-scope yt-formatted-string", l.setAttribute("dir", "auto"), r = document.createTextNode(""), l.appendChild(r), a[1].insertAdjacentElement("afterend", l), t = l, n = " • ") : t = a[2];
                 var r = t.firstChild.nodeValue,
                     l = t.getAttribute("data-text");
-                null !== l && l === r || i(t) && (t.setAttribute("data-text", r), a = d(e.querySelector("a#thumbnail").getAttribute("href")), s && u(a, "video playlist"), o(a, e => {
-                    e = n + c(e), t.firstChild.nodeValue = e, t.setAttribute("data-text", e)
+                null !== l && l === r || isVisble(t) && (t.setAttribute("data-text", r), a = shortsHelper(e.querySelector("a#thumbnail").getAttribute("href")), s && utility(a, "video playlist"), fetchVideoInfo(a, e => {
+                    e = n + LocalizeDates(e), t.firstChild.nodeValue = e, t.setAttribute("data-text", e)
                 }))
             }
         })
@@ -194,7 +180,7 @@ window.addEventListener("load", function() {
         a = 0,
         r;
 
-    function u(e, t) {
+    function utility(e, t) {
         r || ((r = document.createElement("span")).id = "ytud-counter", r.innerText = "0", r.style.cssText = "position: fixed; top: 0; left: 0; width: 30px; height: 30px; background-color: red; color: white; text-align: center; line-height: 30px; font-size: 16px; font-weight: bold; border-radius: 5px; z-index: 9999;", document.body.appendChild(r)), a++, r.textContent = a;
         var n = document.createElement("span");
         n.style.cssText = "position: fixed; left: 0; height: 30px; background-color: black; color: red; text-align: left; line-height: 30px; font-size: 16px; font-weight: bold; border-radius: 5px; z-index: 9999;", n.style.top = 30 * a + "px", n.textContent = e + " " + t, document.body.appendChild(n)
